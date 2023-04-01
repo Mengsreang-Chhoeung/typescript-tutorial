5482f8ef707dac4b231a8b06f61ca9d5535d1dc9
Mengsreang Chhoeung <chhoeungmengsreang789@gmail.com>
2023-04-01 22:20:04 +0700

    update readme

--- app/index.ts
+++
@@ -1,3 +0,0 @@
-const student: string = "I'm a student";
-
-console.log(student);
--- app/variable.ts
+++
@@ -1,9 +0,0 @@
-var num: number = 100;
-let firstName: string = "mengsreang";
-const siblings: string[] = ['thyda', 'kanha', 'seyha'];
-
-firstName = "dyna";
-
-console.log(num);
-console.log(firstName);
-console.log(siblings);
