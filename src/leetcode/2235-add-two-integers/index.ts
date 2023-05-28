2cec995dfa547a04a00aa5b5ce8e73194dbf0603
Mengsreang-Chhoeung <chhoeungmengsreang789@gmail.com>
2023-05-28 18:45:04 +0700

    add leetcode-2235-add-two-integers

---
+++ src/leetcode/2235-add-two-integers/index.ts
@@ -0,0 +1,8 @@
+// declare a function
+function sum(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+// invoke a function
+console.log('Case 1: ', sum(12, 5));
+console.log('Case 2: ', sum(-10, 4));
